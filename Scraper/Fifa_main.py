be1b79a967c74efc0c36e385991b71e4e493f14b
Soumitra <SoumitraAgarwal@github.com>
2017-03-31 01:58:49 +0530

    Name and url extraction code complete

--- Scraper/Fifa_main.py
+++ Scraper/Fifa_main.py
@@ -2,8 +2,9 @@
 from bs4 import BeautifulSoup
 import urllib2
 import requests
+import pandas as pd
 
-url = "https://www.fifaindex.com/"
+url = "https://www.fifaindex.com"
 proxies = {
   'http': 'http://10.4.22.5:3128',
   'https': 'https://10.4.22.5:3128',
@@ -17,7 +18,6 @@ right_table=soup.find('table', class_='table table-striped players')
 
 A=[]
 B=[]
-C=[]
 
 for row in right_table.findAll("tr"):
     cells = row.find("td")
@@ -26,7 +26,35 @@ for row in right_table.findAll("tr"):
     	if(a!=None):
 	    	A.append(a["title"])
 	        B.append(a["href"])
-print(A)
-# print(B)
-# print(C)
+
+temp_df=pd.DataFrame({'Name':A, 'url' : B})
+print(temp_df)
+
+for i in range(2,588):
+	print(i)
+	url_temp = url+'/players/'+str(i)+'/'
+	
+	while(True):
+		print("Getting page "+str(i))
+		try:
+			page = requests.get(url_temp,proxies=proxies)
+		except requests.exceptions.RequestException as e:  # This is the correct syntax
+			print(e)
+			continue
+		break
+
+	html = page.content
+	soup = BeautifulSoup(html,'lxml')
+	right_table=soup.find('table', class_='table table-striped players')
+
+	for row in right_table.findAll("tr"):
+	    cells = row.find("td")
+	    if(cells!=None):
+	    	a = cells.find("a")
+	    	if(a!=None):
+		    	A.append(a["title"])
+		        B.append(a["href"])
+	df=pd.DataFrame({'Name':A, 'url' : B})
+	print(df)
+df.to_csv('Names.csv', index = False, encoding = 'utf-8')
 
