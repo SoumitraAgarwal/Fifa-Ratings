6fffa3ef45914d346c41faf0b49f6c1ab2ee9211
SoumitraAgarwal <agarwalsoumitra1504@gmail.com>
2017-10-01 04:37:59 +0530

    Data commit

--- Female/Combine.py
+++ Female/Combine.py
@@ -2,17 +2,17 @@ import numpy as np
 import cv2
 import os
 
-base = 'Pictures/'
+base = 'Worked/'
 images = os.listdir(base)
-images = images[:100]
+# images = images[:100]
 output = cv2.imread(base+images[0])
 image1 = cv2.imread(base+images[1])
-cv2.addWeighted(image1, 0.012, output, 0.012, 0, output)
+cv2.addWeighted(image1, 1.0/len(images), output, 1.0/len(images), 0, output)
 
 for i in range(2,len(images)):
 
 	# load the image
 	image1 = cv2.imread(base+images[i])
-	cv2.addWeighted(image1, 0.012, output, 1, 0, output)
+	cv2.addWeighted(image1, 1.0/len(images), output, 1, 0, output)
 print(output)
-cv2.imwrite("Output1.jpg", output)
+cv2.imwrite("Fifawomen2.jpg", output)
